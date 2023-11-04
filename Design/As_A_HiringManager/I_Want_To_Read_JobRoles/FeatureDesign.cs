6bfcc425229a2801968911f1c9b5e260df56a370
Aladar <aladar.horvath@mbg.cognizant.com>
2023-11-04 16:16:42 +0000

    refactor test

--- Design/As_A_HiringManager/I_Want_To_Read_JobRoles/FeatureDesign.cs
+++ Design/As_A_HiringManager/I_Want_To_Read_JobRoles/FeatureDesign.cs
@@ -5,12 +5,12 @@ namespace Design.As_A_HiringManager.I_Want_To_Read_JobRoles;
 public class FeatureDesign
 {
     [Fact]
-    public async void Can_Be_Called()
+    public async Task Can_Be_Called()
     {
         var request = new Request();
         var token = CancellationToken.None;
         var feature = new Feature();
         var response = await feature.Run(request, token);
-        Assert.NotNull(response);
+        response.ShouldBe_NotNull();
     }
 }
---
+++ Design/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Design;
+
+public static class Extensions
+{
+    public static T? ShouldBe_NotNull<T>(this T? actual)
+    {
+        Assert.NotNull(actual);
+        return actual;
+    }
+}
