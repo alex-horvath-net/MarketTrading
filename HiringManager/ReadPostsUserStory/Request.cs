88ea41a7f87b84172e4a523e26f7bea3d25fdd48
Aladár Horváth <aladar.horvath@outlook.com>
2023-12-12 16:44:30 +0000

    sdsa

--- HiringManager/ReadPostsUserStory/Request.cs
+++ HiringManager/ReadPostsUserStory/Request.cs
@@ -2,6 +2,8 @@
 
 namespace BloggerUserRole.ReadPostsUserStory;
 
-public record Request(string Title, string Content) : RequestCore
+public record Request(
+    string Title, 
+    string Content) : RequestCore
 {
 }
